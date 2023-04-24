4f28b378190477c76959d5252d9571733a060d94
tekimmehmet <tekimmehmet@hotmail.com>
2023-04-25 01:04:23 +0300

    Context sınıfımızı oluşturduk.

---
+++ ApiConsume/HotelProject.DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,27 @@
+﻿using HotelProject.EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.DataAccessLayer.Concrete
+{
+    public class Context:DbContext
+    {
+        //burada yaptığımız işlem ekleyeceğimiz migration çin bağlantı kısmını ezmek ve istediğimiz bağlantıya gidip bizim için bizim vereceğimiz isimle bir veri tabanı oluşturmasını sağlamak.
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=MEHMETTEKIN\\MSSQL;initial catolog=ApiDb;integrated security=true");// burada dedikki dostum bağlantı adresinin adı şu initial catolog kısmında da veri tabanı adın bu doğrulama ile de gir dedik.
+        }
+
+        //şimdi de entitylerimizi eklemeliyiz ki db içindeki tablolarmız oluşsun
+
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<Staff> Staffs { get; set; }
+        public DbSet<Subscribe> Subscribes { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+    }
+}
