22b9f6973f7e2e6bc77b5907264f10aa718f6a78
tekimmehmet <tekimmehmet@hotmail.com>
2023-04-25 01:28:07 +0300

    Generic yapımızın temelini attık.

---
+++ ApiConsume/HotelProject.DataAccessLayer/Abstract/IGenericDal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.DataAccessLayer.Abstract
+{
+    //Bir T değeri alacağız ve bu T değeri garanti class olmalı
+    public interface IGenericDal<T> where T : class
+    {
+        //şimdi içerisinde Api içerisindeki CRUD işlemleri için metotlarımızı tanımlayalım.
+
+        void Insert(T t);
+        void Delete(T t);
+        void Update(T t);
+        List<T> GetList();
+        T GetById(int id);
+    }
+}
