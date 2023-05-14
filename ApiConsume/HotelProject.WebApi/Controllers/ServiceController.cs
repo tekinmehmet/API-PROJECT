57af13927232abead8809e459ba05c19f2d91bb4
tekimmehmet <tekimmehmet@hotmail.com>
2023-05-15 00:46:49 +0300

    Controller kısmı yazıldı.

---
+++ ApiConsume/HotelProject.WebApi/Controllers/ServiceController.cs
@@ -0,0 +1,55 @@
+﻿using HotelProject.BusinessLayer.Abstract;
+using HotelProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelProject.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ServiceController : ControllerBase
+    {
+        private readonly IServiceService _serviceService;
+
+        public ServiceController(IServiceService serviceService)
+        {
+            _serviceService = serviceService;
+        }
+
+        [HttpGet]
+        public IActionResult ServiceList()
+        {
+            var values = _serviceService.TGetList();
+            return Ok(values);
+        }
+        [HttpPost]
+        public IActionResult AddService(Service service)
+        {
+            _serviceService.TInsert(service);
+            return Ok();
+        }
+        [HttpDelete]
+        public IActionResult DeleteService(int id)
+        {
+            var vulues = _serviceService.TGetById(id);
+            _serviceService.TDelete(vulues);
+            return Ok();
+        }
+        [HttpPut]
+        public IActionResult UpdateService(Service service)
+        {
+            _serviceService.TUpdate(service);
+            return Ok();
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetService(int id)
+        {
+            var values = _serviceService.TGetById(id);
+            return Ok(values);
+        }
+    }
+}
